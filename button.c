9ee90661a14532cace83d36cc8993e08e8900bf9
Nick Schrader <nick.schrader@mailbox.org>
2016-10-25 23:09:55 +0200

    File headers

--- button.c
+++ button.c
@@ -1,3 +1,26 @@
+/*
+ * button.c
+ *
+ * Copyright 2016 Nick Schrader <nick.schrader@mailbox.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ *
+ */
+
 #include "button.h"
 #include "main.h"
 #include "usbdrv.h"
@@ -10,7 +33,7 @@ static uchar lastButton = FALSE;
 void buttonPoll() {
 	uchar sameButtonState = FALSE;
 	uchar button = digitalReadButton();
-	if (button) {
+	if (button){
 		if (!lastButton)
 			buttonPushCycleCount = cycleCount;
 		int32_t diff = cycleCount - buttonPushCycleCount;
