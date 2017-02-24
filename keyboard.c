c82651d0603ad6efb520584e8ef062ad923d7914
Nick Schrader <nick.schrader@mailbox.org>
2017-02-24 17:31:43 +0100

    Auto Enter implementation + a little refactoring

--- keyboard.c
+++ keyboard.c
@@ -1,24 +1,24 @@
 /*
  * keyboard.c
- * 
+ *
  * Copyright 2016 Nick Schrader <nick.schrader@mailbox.org>
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA 02110-1301, USA.
- * 
- * 
+ *
+ *
  */
 
 
@@ -33,19 +33,21 @@
 #include "passwordSeed.h"
 #include "random.h"
 #include "conversionTable.h"
- 
+
 #define CAPS_LOCK_LED 0x02
 #define CAPS_LOCK_KEY 0x39
+
+#define ENTER_KEY	0x28
 
 #define SHIFT_MODIFIER 0x20
 #define NO_MODIFIER 0x00
 
-static const __flash uint8_t *letterConversionTable;
+static const __flash uint8_t* letterConversionTable;
 
-static uint8_t correctLetter(passwordSeed *s) {
+static uint8_t correctLetter(passwordSeed* s) {
 	if (menuPage == LANG_EN)
 		return TRUE;
-	for(uint8_t i = 0; i < letterConversionTableLenght; i++) {
+	for (uint8_t i = 0; i < letterConversionTableLenght; i++) {
 		if (s->keycode == letterConversionTableEN[i]) {
 			s->keycode = letterConversionTable[i];
 			return TRUE;
@@ -61,7 +63,7 @@ static uint8_t correctLetter(passwordSeed *s) {
  * DE	1-0
  */
 
-static uint8_t correctDigit(passwordSeed *s) {
+static uint8_t correctDigit(passwordSeed* s) {
 	if (menuPage == LANG_DE || menuPage == LANG_EN)
 		return TRUE;
 	if (s->modifier == 0 && s->keycode >= KEYCODE_1 && s->keycode <= KEYCODE_0) {
@@ -71,12 +73,12 @@ static uint8_t correctDigit(passwordSeed *s) {
 	return FALSE;
 }
 
-static const __flash uint8_t *signConversionTable;
+static const __flash uint8_t* signConversionTable;
 
-static uint8_t correctSign(uint8_t *s) {
+static uint8_t correctSign(uint8_t* s) {
 	if (menuPage == LANG_EN)
 		return TRUE;
-	for(uint8_t i = 0; i < signConversionTableLenght; i++) {
+	for (uint8_t i = 0; i < signConversionTableLenght; i++) {
 		if (*s == signConversionTableEN[i]) {
 			*s = signConversionTable[i];
 			return TRUE;
@@ -84,35 +86,43 @@ static uint8_t correctSign(uint8_t *s) {
 			return FALSE;
 	}
 	return FALSE;
-} 
+}
 
 uint8_t messageState = STATE_DONE;
-static void * messagePtr = NULL;
+static void* messagePtr = NULL;
 static uint8_t messageCharNext = TRUE;
 static uint8_t messageRestoreCapsLock = FALSE;
+static uint8_t messageSentEnter = TRUE;
 
 uint8_t buildReport() {
-	if (messageState == STATE_DONE || messagePtr >= NULL + PASSWORD_LENGTH*3) { // End of transmission
-		if (messageRestoreCapsLock){
+	if (messageState == STATE_DONE || messagePtr >= NULL + PASSWORD_LENGTH * 3) { // End of transmission
+		if (!messageSentEnter) {
+			keyboardReport.modifier = NO_MODIFIER;
+			keyboardReport.keycode[0] = ENTER_KEY;
+			messageSentEnter = TRUE;
+			return STATE_SEND;
+		}
+		if (messageRestoreCapsLock) {
 			keyboardReport.modifier = NO_MODIFIER;
 			keyboardReport.keycode[0] = CAPS_LOCK_KEY;
 			messageRestoreCapsLock = FALSE;
 			LedState |= CAPS_LOCK_LED;
 			return STATE_SEND;
 		}
-		memset(&keyboardReport, 0, sizeof (keyboardReport));
+		memset(&keyboardReport, 0, sizeof(keyboardReport));
 		return STATE_DONE;
 	}
-	if (LedState & CAPS_LOCK_LED){ // unlocks kaps
+	messageSentEnter = FALSE;
+	if (LedState & CAPS_LOCK_LED) { // unlocks kaps
 		keyboardReport.modifier = NO_MODIFIER;
 		keyboardReport.keycode[0] = CAPS_LOCK_KEY;
 		messageRestoreCapsLock = TRUE;
 		LedState ^= CAPS_LOCK_LED;
 		return STATE_SEND;
 	}
-	if (messageCharNext){ // send a keypress
+	if (messageCharNext) { // send a keypress
 		passwordSeed s;
-		eeprom_read_block(&s, NULL + random2(), sizeof (passwordSeed));
+		eeprom_read_block(&s, NULL + random2(), sizeof(passwordSeed));
 		if (!correctSign((uint8_t*) &s)) {
 			if (!correctLetter(&s)) {
 				correctDigit(&s);
@@ -122,23 +132,23 @@ uint8_t buildReport() {
 		keyboardReport.keycode[0] = s.keycode;
 		messagePtr++;
 	} else // send a keyrelease
-		memset(&keyboardReport, 0, sizeof (keyboardReport));
+		memset(&keyboardReport, 0, sizeof(keyboardReport));
 	messageCharNext = !messageCharNext; // invert
 	return STATE_SEND;
 }
 
 void startTransmission() {
-	messagePtr = NULL + PASSWORD_LENGTH*2;
+	messagePtr = NULL + PASSWORD_LENGTH * 2;
 	messageState = STATE_SEND;
 	srandom2(getPIN());
 	if (menuPage == LANG_EN) {
 		letterConversionTable = letterConversionTableEN;
 		signConversionTable = signConversionTableEN;
-	}  else if (menuPage == LANG_DE) { 
+	} else if (menuPage == LANG_DE) {
 		letterConversionTable = letterConversionTableDE;
 		signConversionTable = signConversionTableDE;
 	} else { // language == LANG_FR
 		letterConversionTable = letterConversionTableFR;
 		signConversionTable = signConversionTableFR;
-	}	
+	}
 }
